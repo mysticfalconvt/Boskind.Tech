f8b4dd92d688010f60af9266c63c08b57dab0817
mysticfalconvt <rboskind@gmail.com>
2020-03-22 19:52:48 -0400

    nav almost working

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+console.log('connected');
 document.addEventListener('DOMContentLoaded', function() {
+	console.log('dom content loaded');
 	var elems = document.querySelectorAll('.sidenav');
 	var instances = M.Sidenav.init(elems, options);
+	console.log('asdf');
 });
