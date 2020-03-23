595d27543caa3b5865909ec4043fd9e6df5b390d
mysticfalconvt <rboskind@gmail.com>
2020-03-23 19:54:22 -0400

    side nav

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 console.log('connected');
-document.addEventListener('load', function() {
-	var elems = document.querySelectorAll('.sidenav');
-	var instances = M.sidenav.init(elems, {});
+// document.addEventListener('load', function() {
+// 	var elems = document.querySelectorAll('.sidenav');
+// 	var instances = M.sidenav.init(elems, {});
+// });
+
+// function doSomething() {
+// 	console.info('DOM loaded');
+// }
+
+// if (document.readyState === 'loading') {
+// 	// Loading hasn't finished yet
+// 	document.addEventListener('DOMContentLoaded', doSomething);
+// } else {
+// 	// `DOMContentLoaded` has already fired
+// 	doSomething();
+// }
+$(document).ready(function() {
+	$('.sidenav').sidenav();
 });
-
-function doSomething() {
-	console.info('DOM loaded');
-}
-
-if (document.readyState === 'loading') {
-	// Loading hasn't finished yet
-	document.addEventListener('DOMContentLoaded', doSomething);
-} else {
-	// `DOMContentLoaded` has already fired
-	doSomething();
-}
