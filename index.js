ebfa1864192f9892cae8e5dc871382ad465331e3
mysticfalconvt <rboskind@gmail.com>
2020-04-02 15:30:50 -0400

    added yoda

--- index.js
+++ index.js
@@ -18,3 +18,50 @@ console.log('connected');
 $(document).ready(function() {
 	$('.sidenav').sidenav();
 });
+
+const turtle = document.querySelector('.turt');
+let x = 0;
+let y = 0;
+const speed = 50;
+let flipped = false;
+let rotate = 0;
+function handleKeyDown(event) {
+	// if its not an arrow key, we dont care
+	if (!event.key.includes('Arrow')) {
+		return;
+	}
+	switch (event.key) {
+		case 'ArrowUp':
+			y = y - 1;
+			rotate = 90;
+			break;
+		case 'ArrowDown':
+			y = y + 1;
+			rotate = -90;
+			break;
+		case 'ArrowLeft':
+			x = x - 1;
+			rotate = 0;
+			flipped = true;
+			break;
+		case 'ArrowRight':
+			x = x + 1;
+			rotate = 0;
+			flipped = false;
+			break;
+		default:
+			console.log('That is not a valid move');
+			break;
+	}
+
+	turtle.setAttribute(
+		'style',
+		`
+        --rotateX: ${flipped ? '0' : '180deg'};
+        --x: ${x * speed}px;
+        --y: ${y * speed}px;
+        --rotate: ${rotate}deg;
+      `
+	);
+}
+window.addEventListener('keydown', handleKeyDown);
